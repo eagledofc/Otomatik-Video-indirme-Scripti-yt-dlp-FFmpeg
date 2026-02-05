15ad648d60eca1c363407278f8527b538abbd41a
eagledofc <eagledofc@gmail.com>
2026-02-05 13:27:39 +0300

    Refactor video downloader batch script

--- OFC Video İndirme Aracı/Vİdeo İndirme.bat
+++ OFC Video İndirme Aracı/Vİdeo İndirme.bat
@@ -1,8 +1,3 @@
-diff --git a/OFC Video İndirme Aracı/Vİdeo İndirme.bat b/OFC Video İndirme Aracı/Vİdeo İndirme.bat
-index d79eede31b74c484a3207ee041425467afe923b3..377857c3e7fbc10b402e2ad7aaaaac0c8eecc048 100644
---- a/OFC Video İndirme Aracı/Vİdeo İndirme.bat	
-+++ b/OFC Video İndirme Aracı/Vİdeo İndirme.bat	
-@@ -1,68 +1,143 @@
  @echo off
  chcp 65001 >nul
  setlocal enabledelayedexpansion
@@ -184,3 +179,4 @@ index d79eede31b74c484a3207ee041425467afe923b3..377857c3e7fbc10b402e2ad7aaaaac0c
  echo İNDİRME TAMAMLANDI - Dosya konumu: %OUTDIR%
  pause
  exit /b
+
